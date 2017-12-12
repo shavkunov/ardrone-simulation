db80f8b3082de05689d458288d3cc8da2613fe4d
shavkunov <mv.shavkunov@yandex.ru>
2017-12-12 21:35:31 +0300

    fixed js style

--- src/drone.py
+++ src/drone.py
@@ -1,33 +1,35 @@
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String 
-from std_msgs.msg import Empty 
+from std_msgs.msg import String
+from std_msgs.msg import Empty
 from ardrone_autonomy.msg import Navdata
 
 class Drone():
-    def __init__(self):
-        self.status = ""
+    def __init__(self, navdataListener):
         rospy.init_node('ardrone_flight', anonymous=False)
         self.rate = rospy.Rate(10)
         self.pubTakeoff = rospy.Publisher("ardrone/takeoff",Empty)
         self.pubLand = rospy.Publisher("ardrone/land",Empty)
         self.pubCommand = rospy.Publisher('cmd_vel',Twist)
         self.command = Twist()
-        self.navdata = rospy.Subscriber("ardrone/navdata", Navdata, self.NavdataCallback)
-        self.state_change_time = rospy.Time.now()    
-        rospy.on_shutdown(self.Land)
+        self.navdata = rospy.Subscriber("ardrone/navdata", Navdata, navdataListener)
+        self.state_change_time = rospy.Time.now()
+        rospy.on_shutdown(self.land)
 
-    def NavdataCallback(self, navdata):
+    def navdataCallback(self, navdata):
         time = navdata.header.stamp.to_sec()
         # navigation data proccessing
 
-    def TakeOff(self):
-        self.pubTakeoff.publish(Empty()) 
+    def takeOff(self):
+        self.pubTakeoff.publish(Empty())
         self.rate.sleep()
-                
-    def Land(self):
+
+    def land(self):
         self.pubLand.publish(Empty())
-    
-        
+
+    def stop(self):
+        self.Command(0, 0, 0, 0, 0, 0)
+
+
     def Command(self, linear_x, linear_y, linear_z, angular_x, angular_y, angular_z):
         self.command.linear.x = linear_x
         self.command.linear.y = linear_y
--- src/pid.py
+++ src/pid.py
@@ -15,13 +15,13 @@ class PID(self, kp, ki, kd):
         self.reset()
 
     def reset(self):
-        self._last_time = 0;
+        self._last_time = 0
         self._last_error = Infinity
-        self._error_sum = 0;
+        self._error_sum = 0
 
-    def getCommand(e):
+    def getCommand(self, e):
         # Compute dt in seconds
-        time = calendar.timegm(time.gmtime()) # calculates elapsed seconds from 1970
+        time = getDateNow()
         dt = (time - self._last_time) / 1000
 
         de = 0
@@ -31,13 +31,17 @@ class PID(self, kp, ki, kd):
                 de = (e - self._last_error) / dt;
 
             # Integrate error
-            self._error_sum += e * dt;
+            self._error_sum += e * dt
         }
 
         # Update our trackers
-        self._last_time = time;
-        self._last_error = e;
+        self._last_time = time
+        self._last_error = e
 
         # Compute commands
         command = self._kp * e + self._ki * self._error_sum + self._kd * de
         return command
+
+# calculates elapsed seconds from 1970
+def getDateNow():
+    return calendar.timegm(time.gmtime())
