1a617e015936b9b5758b16a652abfa2f397263d4
shavkunov <mv.shavkunov@yandex.ru>
2018-02-09 02:44:54 +0000

    removed trash

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osboxes/tum_simulator_ws/src/python_script
-# Build directory: /home/osboxes/tum_simulator_ws/src/python_script
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_generated/ordered_paths.cmake
+++
@@ -1,1 +0,0 @@
-set(ORDERED_PATHS "/home/osboxes/workspace/ros/install_isolated/lib")
